9e37c7938b6962f027ea2d2e9c7b450fd1c45ced
hungdt <hungdt>
2020-04-01 23:41:07 +0700

    [FIX] Authentication

--- functions/services/auth.js
+++ functions/services/auth.js
@@ -7,24 +7,21 @@ exports.createUserForAuthentication = functions.auth
     const email = user.email // The email of the user.
     const name = user.displayName // The display name of the user.
     const batch = admin.firestore().batch()
-    try {
-      const userRef = admin
-        .firestore()
-        .collection('users')
-        .doc(`${user.uid}`)
-      batch.set(userRef, {
-        email,
-        name,
-        balance: 0
-      })
-      const historyRef = admin
-        .firestore()
-        .collection('history')
-        .doc(`${user.uid}`)
-      batch.set(historyRef, {
-        created: 0
-      })
-      return batch.commit()
-    } catch (err) {
-    }
+    const userRef = admin
+      .firestore()
+      .collection('users')
+      .doc(`${user.uid}`)
+    batch.set(userRef, {
+      email,
+      name,
+      balance: 0
+    })
+    const historyRef = admin
+      .firestore()
+      .collection('history')
+      .doc(`${user.uid}`)
+    batch.set(historyRef, {
+      created: 0
+    })
+    return batch.commit()
   })
